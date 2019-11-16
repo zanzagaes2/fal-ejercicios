21ce2f87f718b7ca9658018debd0dca1a5d499eb
Alvaro <asanzramos@gmail.com>
2019-11-16 19:14:38 +0100

    Arreglado ejercicio 6

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -1,11 +1,11 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+
+
 
 /*
 Coste del algoritmo (tiempo): O(n)
 Coste del algoritmo (espacio): O(n)
-
 Precondiciones: N > 0
 fun resuelveCaso(V[N]: de ent) dev maxLongitudSegmento
 Poscondiciones:
@@ -13,38 +13,47 @@ Poscondiciones:
 Invariante:
     maxLongitudSegmento = (max j,k: 0 < j < k < i ^ (para todo l: j <= l < k: V[k] = V[k+1]) : j -k)
     longitud = i - (min j : 0 < j < i: (para todo k: j <= k < i: V[k] = V[k+1]))
-
 Avanzar: i++
     maxLongitudSegmento = max { maxLongitudSegmento, longitud }
                         
-FunciÃ³n de cota: N - i 
-CondiciÃ³n de bucle: i < N
-TerminaciÃ³n: i = N
+Función de cota: N - i 
+Condición de bucle: i < N
+Terminación: i = N
 */
 
-long int resuelveCaso(std::vector<long int> &v) {
-    long int longitudSegmento = 0, maxLongitudSegmento = 0; 
-    long unsigned int size = v.size();
-    for(long unsigned int i = 0; i < size-1; i++)
-        if (v.at(i) == v.at(i+1) && ++longitudSegmento > maxLongitudSegmento) 
-            maxLongitudSegmento = longitudSegmento;
-        else longitudSegmento = 0;
-    return maxLongitudSegmento;
+bool resuelve();
+long int resuelveCaso(const std::vector<int> &v);
+
+bool resuelve() {
+	int nuevoNumero, respuesta;
+  std::vector <int> v;
+
+  std::cin >> nuevoNumero;
+  if (nuevoNumero == -1) return false; 
+	while (nuevoNumero != -1) {
+		v.push_back(nuevoNumero);
+    std::cin >> nuevoNumero;
+	}
+
+	respuesta = resuelveCaso(v);
+	if (respuesta == 0) std::cout << "HOY NO COMEN" << std::endl;
+	else std::cout << respuesta << std::endl;
+
+	return true;
+}
+long int resuelveCaso(const std::vector<int> &v) {
+	long int maxLongitudSegmento = 0, longitudSegmento = 0;
+
+	for (long int i = 1; i < v.size(); ++i) {
+		if (v[i - 1] == v[i]) ++longitudSegmento;
+		else longitudSegmento = 0; 
+		if (longitudSegmento > maxLongitudSegmento) 
+      maxLongitudSegmento  = longitudSegmento;
+	}
+  return maxLongitudSegmento;
 }
 
 int main() {
-    do {
-        long int datoLeido;
-        std::vector<long int> v;
-        std::cin >> datoLeido;
-        while (datoLeido != -1) {
-            v.push_back(datoLeido);
-            std::cin >> datoLeido;
-        }
-        if (v.size()) {
-            long int diff = resuelveCaso(v);
-            if (diff == 0) std::cout << "HOY NO COMEN" << std::endl;
-            else std::cout << diff << std::endl;
-        }
-	} while (std::cin);
+	while (resuelve());
+	return 0;
 }
